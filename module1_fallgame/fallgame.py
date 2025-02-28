064507df772f5ce0aa3b320aa829d051abdbfb5b
omarburnett <omargithub5545@gmail.com>
2025-02-28 10:23:17 -0500

    created player and falling obect

--- module1_fallgame/fallgame.py
+++ module1_fallgame/fallgame.py
@@ -8,26 +8,35 @@ pygame.display.set_caption('avoid falling stuff')
 clock = pygame.time.Clock()
 fps=60
 running=True
-while running:
-    for event in pygame.event.get():
-
-        if event.type==pygame.quit:
-            running=False
-  
-pygame.quit()
-
 #objects are just values
 class player:
-    def_init_(self)
-    self.x = width // 2
-    self.y= height -60
-    self.playerwidth =50
-    self.playerheight=50
-    self.playerspeed=5
+    def _init_(self):
+        self.x = width // 2
+        self.y= height -60
+        self.playerwidth =50
+        self.playerheight=50
+        self.playerspeed=5
 def move(self,keys):
     if keys[pygame.l_LEFT]and self.x > 0:
         self.x -=self.playerspeed
     if keys[pygame.K_RIGHT]and self.x < width - self.playerwidth:
         self.x+=self.playerspeed
 def draw(self):
-    pygame.draw.rect(screen,(0,0,225),(self.x,self.y self.width self.height))
+    pygame.draw.rect(screen,(0,0,225),
+    (self.x,self.y self.width self.height))
+class fallingobjects:
+    def __init__(self):
+        self.x= random.randint(0,width - 50)
+        self.y = -50
+        self.width=50
+        self.height=50
+        self.speed=random.randint(3,7)
+    def move(self):
+        self.y += self.speed
+     pygame.draw.rect(screen,(0,0,225),(self.x,self.y self.width self.height))
+while running:
+    for event in pygame.event.get():
+
+        if event.type==pygame.quit:
+            running=False
+pygame.quit()
