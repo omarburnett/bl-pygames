d8333f4b79d2caf350ebb99992e2c937fbff2a8e
omarburnett <omargithub5545@gmail.com>
2025-03-05 10:25:39 -0500

    falling objects dynamics 3/5/25

--- module1_fallgame/fallgame.py
+++ module1_fallgame/fallgame.py
@@ -1,29 +1,37 @@
 import pygame
 import random
+
 pygame.init()
-height=1000
-width=1200
+height= 1000
+width= 1200
+
 screen=pygame.display.set_mode((width,height))
+
 pygame.display.set_caption('avoid falling stuff')
+
 clock = pygame.time.Clock()
-fps=60
+fps= 60
+
 running=True
 #objects are just values
-class player:
+
+class Player:
     def _init_(self):
         self.x = width // 2
         self.y= height -60
         self.playerwidth =50
         self.playerheight=50
         self.playerspeed=5
-def move(self,keys):
-    if keys[pygame.l_LEFT]and self.x > 0:
-        self.x -=self.playerspeed
-    if keys[pygame.K_RIGHT]and self.x < width - self.playerwidth:
-        self.x+=self.playerspeed
-def draw (self):
-    pygame.draw.rect(screen,(0,0,225),
-    (self.x,self.y self.width self.height))
+    
+    def move(self,keys):
+        if keys[pygame.l_LEFT]and self.x > 0:
+            self.x -=self.playerspeed
+        if keys[pygame.K_RIGHT]and self.x < width - self.playerwidth:
+            self.x+=self.playerspeed
+    
+    def draw (self):
+        pygame.draw.rect(screen,(0,0,225), (self.x, self.y,self.playerwidth, self.playerheight))
+
 class fallingobjects:
     def __init__(self):
         self.x= random.randint(0,width - 50)
@@ -33,22 +41,34 @@ class fallingobjects:
         self.speed=random.randint(3,7)
     def move(self):
         self.y += self.speed
-     pygame.draw.rect(screen,(0,0,225),(self.x,self.y self.width self.height))
-player =player()
+        pygame.draw.rect(screen,(0,0,225),(self.x,self.y, self.width, self.height))
+
+player = Player()
 falling_object=[]
 score=0
 lives=3
+
 while running:
     clock.tick(fps)
-screen.fill(white)
-keys=pygame.key.event.get()
-player.move+
+    screen.fill(white)
+    keys=pygame.key.get_pressed()
+    player.move+
 
     for event in pygame.event.get():
 
         if event.type==pygame.quit:
             running=False
-pygame.quit()
+            if random.randint(1,30)==1:
+                falling_object.append(falling_object())
+    for obj in fallingobjects[:]:
+        obj.move()
+        obj.draw()
+        for event in pygame.event.get():
+            if event.type ==
+    if(obj .x< player.x + player.playerwidth and
+    obj.x + obj .width >player .player.x and 
+    obj.y < player)      
 
+pygame.quit()
 
 # instructor note
